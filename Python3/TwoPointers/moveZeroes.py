e6f53f8e6cc14e35d6b44a651c39749761116fe9
Md Mahbubur Rahman <MarufSAir@Users-MacBook-Air.local>
2024-01-11 22:37:24 -0400

    This commmit consists List of the Leet Code problems

---
+++ Python3/TwoPointers/moveZeroes.py
@@ -0,0 +1,42 @@
+'''
+283. Move Zeroes
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array. 
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+'''
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = 0
+        for i in range(n):
+            if nums[i] != 0:
+                nums[k] = nums[i]
+                k += 1
+        for i in range(k, n):
+            nums[i] = 0
+
+
+'''
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[fast], nums[slow] = nums[slow], nums[fast]
+            if nums[slow] != 0:
+                slow += 1
+'''
